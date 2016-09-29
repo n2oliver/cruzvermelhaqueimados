396e6573bbfc5ff8c354f2d28f7ca390c4e8f4c9
ProjetoEVivoEBD <projetoevivoebd@gmail.com>
2016-09-28 22:39:09 -0300

    Tela de login pronta

--- admin.php
+++ admin.php
@@ -1,5 +1,18 @@
 <?php include("header.php"); ?>
-<h1>Você é um administrador?</h1>
+<h3>Se você é um administrador,</h3>
+<p style="align:center; color:red; font-weight:bold">então, faça o seu login:</p>
+<form>
+<hr>
+<input type="hidden">
+<label for="nome">Nome:</label>
+<input id="nome" class="form-control">
+<br>
+<label for="senha">Senha:</label>
+<input id="senha" class="form-control">
+<hr>
+<input type="submit" value="Logar" id="button" class="btn btn-primary">
+</form>
 </div>
-
-<?php include("footer.php");?>
+</div>
+</body>
+</html>
